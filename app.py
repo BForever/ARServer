8642cbf041a37e9b8c94352f4a3218092a9de5fc
BForever <11fhc@163.com>
2019-09-29 18:27:44 +0800

    dont reload

--- app.py
+++ app.py
@@ -8,7 +8,24 @@ import logging
 import darknet.python.darknet as darknet
 
 app = Flask(__name__)
+logger = app.logger
+logger.setLevel(logging.DEBUG)
+file_log_handler = logging.FileHandler("log")
+file_log_handler.setLevel(logging.INFO)
+file_log_handler.setFormatter(logging.Formatter(
+    '%(asctime)s - %(levelname)s -%(pathname)s- %(filename)s - %(funcName)s - %(lineno)s - %(message)s'))
+logger.addHandler(file_log_handler)
+start = time.time()
+if len(sys.argv) <= 1:
+    net = darknet.load_net("darknet/cfg/yolov3-tiny.cfg".encode('utf-8'),
+                           "darknet/yolov3-tiny.weights".encode('utf-8'), 0)
+else:
+    net = darknet.load_net("darknet/cfg/yolov3.cfg".encode('utf-8'),
+                           "darknet/yolov3.weights".encode('utf-8'), 0)
 
+meta = darknet.load_meta("darknet/cfg/coco.data".encode('utf-8'))
+end = time.time()
+logger.info("Time used for loading model: {}s".format(end - start))
 
 @app.route('/')
 def hello_world():
@@ -33,22 +50,4 @@ def predict():
 
 
 if __name__ == '__main__':
-    logger = app.logger
-    logger.setLevel(logging.DEBUG)
-    file_log_handler = logging.FileHandler("log")
-    file_log_handler.setLevel(logging.INFO)
-    file_log_handler.setFormatter(logging.Formatter(
-        '%(asctime)s - %(levelname)s -%(pathname)s- %(filename)s - %(funcName)s - %(lineno)s - %(message)s'))
-    logger.addHandler(file_log_handler)
-    start = time.time()
-    if len(sys.argv) <= 1:
-        net = darknet.load_net("darknet/cfg/yolov3-tiny.cfg".encode('utf-8'),
-                               "darknet/yolov3-tiny.weights".encode('utf-8'), 0)
-    else:
-        net = darknet.load_net("darknet/cfg/yolov3.cfg".encode('utf-8'),
-                               "darknet/yolov3.weights".encode('utf-8'), 0)
-
-    meta = darknet.load_meta("darknet/cfg/coco.data".encode('utf-8'))
-    end = time.time()
-    logger.info("Time used for loading model: {}s".format(end - start))
-    app.run("0.0.0.0", 5000, True)
+    app.run("0.0.0.0", 5000, True, use_reloader=False)
