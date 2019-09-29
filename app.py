294ca6fa4481ddc4c81ed91d8ef81d1eae8ca571
BForever <11fhc@163.com>
2019-09-29 17:07:38 +0800

    add model

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import time
+import sys
 from flask import g
 from flask import current_app
 from flask import request
@@ -7,6 +9,7 @@ import darknet.python.darknet as darknet
 
 app = Flask(__name__)
 
+
 logger = app.logger
 logger.setLevel(logging.DEBUG)
 file_log_handler = logging.FileHandler("log")
@@ -14,10 +17,17 @@ file_log_handler.setLevel(logging.INFO)
 file_log_handler.setFormatter(logging.Formatter(
     '%(asctime)s - %(levelname)s -%(pathname)s- %(filename)s - %(funcName)s - %(lineno)s - %(message)s'))
 logger.addHandler(file_log_handler)
+start = time.time()
+if len(sys.argv)<=1:
+    net = darknet.load_net("darknet/cfg/yolov3-tiny.cfg".encode('utf-8'),
+                           "darknet/yolov3-tiny.weights".encode('utf-8'), 0)
+else:
+    net = darknet.load_net("darknet/cfg/yolov3.cfg".encode('utf-8'),
+                           "darknet/yolov3.weights".encode('utf-8'), 0)
 
-net = darknet.load_net("darknet/cfg/yolov3-tiny.cfg".encode('utf-8'),
-                       "darknet/yolov3-tiny.weights".encode('utf-8'), 0)
 meta = darknet.load_meta("darknet/cfg/coco.data".encode('utf-8'))
+end = time.time()
+logger.info("Time used for loading model: {}s".format(end - start))
 
 
 @app.route('/')
@@ -27,12 +37,20 @@ def hello_world():
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    start = time.time()
     with open("temp.jpg", 'wb')as image:
         image.write(request.stream.read())
-
+    end = time.time()
+    logger.info(
+        "Time used for writing image: {}ms".format((end - start) * 1000))
+    start = time.time()
     r = darknet.detect(net, meta, "temp.jpg".encode('utf-8'))
+    end = time.time()
+    logger.info(
+        "Time used for detect: {}ms, result: {}".format((end - start) * 1000,
+                                                        str(r)))
     return str(r)
 
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", 80, True)
+    app.run("0.0.0.0", 5000, True)
--- darknet/python/darknet.py
+++ darknet/python/darknet.py
@@ -1,27 +1,31 @@
 from ctypes import *
 import math
 import random
+from collections import OrderedDict
 
 def sample(probs):
     s = sum(probs)
-    probs = [a/s for a in probs]
+    probs = [a / s for a in probs]
     r = random.uniform(0, 1)
     for i in range(len(probs)):
         r = r - probs[i]
         if r <= 0:
             return i
-    return len(probs)-1
+    return len(probs) - 1
+
 
 def c_array(ctype, values):
-    arr = (ctype*len(values))()
+    arr = (ctype * len(values))()
     arr[:] = values
     return arr
+
 
 class BOX(Structure):
     _fields_ = [("x", c_float),
                 ("y", c_float),
                 ("w", c_float),
                 ("h", c_float)]
+
 
 class DETECTION(Structure):
     _fields_ = [("bbox", BOX),
@@ -38,14 +42,15 @@ class IMAGE(Structure):
                 ("c", c_int),
                 ("data", POINTER(c_float))]
 
+
 class METADATA(Structure):
     _fields_ = [("classes", c_int),
                 ("names", POINTER(c_char_p))]
 
-    
 
-#lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
-lib = CDLL("/Users/fanhongchang/code/ARServer/darknet/libdarknet.so", RTLD_GLOBAL)
+# lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
+lib = CDLL("/Users/fanhongchang/code/ARServer/darknet/libdarknet.so",
+           RTLD_GLOBAL)
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
 lib.network_height.argtypes = [c_void_p]
@@ -63,7 +68,8 @@ make_image.argtypes = [c_int, c_int, c_int]
 make_image.restype = IMAGE
 
 get_network_boxes = lib.get_network_boxes
-get_network_boxes.argtypes = [c_void_p, c_int, c_int, c_float, c_float, POINTER(c_int), c_int, POINTER(c_int)]
+get_network_boxes.argtypes = [c_void_p, c_int, c_int, c_float, c_float,
+                              POINTER(c_int), c_int, POINTER(c_int)]
 get_network_boxes.restype = POINTER(DETECTION)
 
 make_network_boxes = lib.make_network_boxes
@@ -114,6 +120,7 @@ predict_image = lib.network_predict_image
 predict_image.argtypes = [c_void_p, IMAGE]
 predict_image.restype = POINTER(c_float)
 
+
 def classify(net, meta, im):
     out = predict_image(net, im)
     res = []
@@ -122,12 +129,14 @@ def classify(net, meta, im):
     res = sorted(res, key=lambda x: -x[1])
     return res
 
+
 def detect(net, meta, image, thresh=.5, hier_thresh=.5, nms=.45):
     im = load_image(image, 0, 0)
     num = c_int(0)
     pnum = pointer(num)
     predict_image(net, im)
-    dets = get_network_boxes(net, im.w, im.h, thresh, hier_thresh, None, 0, pnum)
+    dets = get_network_boxes(net, im.w, im.h, thresh, hier_thresh, None, 0,
+                             pnum)
     num = pnum[0]
     if (nms): do_nms_obj(dets, num, meta.classes, nms);
 
@@ -136,22 +145,22 @@ def detect(net, meta, image, thresh=.5, hier_thresh=.5, nms=.45):
         for i in range(meta.classes):
             if dets[j].prob[i] > 0:
                 b = dets[j].bbox
-                res.append((meta.names[i], dets[j].prob[i], (b.x, b.y, b.w, b.h)))
+                res.append(
+                    (meta.names[i].decode('utf-8'), dets[j].prob[i], b.x, b.y, b.w, b.h))
     res = sorted(res, key=lambda x: -x[1])
     free_image(im)
     free_detections(dets, num)
     return res
-    
+
+
 if __name__ == "__main__":
-    #net = load_net("cfg/densenet201.cfg", "/home/pjreddie/trained/densenet201.weights", 0)
-    #im = load_image("data/wolf.jpg", 0, 0)
-    #meta = load_meta("cfg/imagenet1k.data")
-    #r = classify(net, meta, im)
-    #print r[:10]
+    # net = load_net("cfg/densenet201.cfg", "/home/pjreddie/trained/densenet201.weights", 0)
+    # im = load_image("data/wolf.jpg", 0, 0)
+    # meta = load_meta("cfg/imagenet1k.data")
+    # r = classify(net, meta, im)
+    # print r[:10]
     net = load_net("cfg/yolov3-tiny.cfg".encode('utf-8'),
-                           "yolov3-tiny.weights".encode('utf-8'), 0)
+                   "yolov3-tiny.weights".encode('utf-8'), 0)
     meta = load_meta("cfg/coco.data".encode('utf-8'))
     r = detect(net, meta, "data/dog.jpg".encode('utf-8'))
     print(r)
-    
-
